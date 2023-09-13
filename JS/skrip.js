36aef79a23db182648e4af5beb62e2b09ab13905
Toriqul-FDW-1 <idhofirdauz11@gmail.com>
2023-09-13 11:18:31 +0800

    t

--- JS/skrip.js
+++ JS/skrip.js
@@ -10,3 +10,44 @@ function dataPerson(){
     `;
     document.getElementById('hasil').innerHTML = data;
 }
+
+
+
+// kalkulator latihan 13
+function tambah(){
+    var frm = document.getElementById('kalkulator');
+    var a1 = parseFloat(frm.angka1.value);
+    var a2 = parseFloat(frm.angka2.value);
+    var total = a1 + a2;
+    frm.hasil.value = total;
+}
+function kurang(){
+    var frm = document.getElementById('kalkulator');
+    var a1 = parseFloat(frm.angka1.value);
+    var a2 = parseFloat(frm.angka2.value);
+    var total = a1 - a2;
+    frm.hasil.value = total;
+}
+function bagi(){
+    var frm = document.getElementById('kalkulator');
+    var a1 = parseFloat(frm.angka1.value);
+    var a2 = parseFloat(frm.angka2.value);
+    var total = a1 / a2;
+    frm.hasil.value = total;
+}
+function kali(){
+    var frm = document.getElementById('kalkulator');
+    var a1 = parseFloat(frm.angka1.value);
+    var a2 = parseFloat(frm.angka2.value);
+    var total = a1 * a2;
+    frm.hasil.value = total;
+}
+function pangkat(){
+    var frm = document.getElementById('kalkulator');
+    var a1 = parseFloat(frm.angka1.value);
+    var a2 = parseFloat(frm.angka2.value);
+    var total = a1 ** a2;
+    frm.hasil.value = total;
+}
+
+
